a37bc20910bea4be7c9ebd6a808a34bbdb42312b
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2024-03-16 02:17:23 +0530

    updated

---
+++ app/component/Banner.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { useState } from 'react';
+import Image from "next/image";
+
+const Banner = () => {
+    const [displayTextIndex, setDisplayTextIndex] = useState(0);
+    const displayTexts = ["Get 10% off on business sign up", "Advertise your message here", "This is developed by Raghav Sharma"];
+
+    const handleClickLeft = () => {
+        const newIndex = (displayTextIndex - 1 + displayTexts.length) % displayTexts.length;
+        setDisplayTextIndex(newIndex);
+    };
+
+    const handleClickRight = () => {
+        const newIndex = (displayTextIndex + 1) % displayTexts.length;
+        setDisplayTextIndex(newIndex);
+    };
+
+    return (
+        <div className="bg-[#F4F4F4] p-2 flex gap-5 items-center justify-center">
+            <button onClick={handleClickLeft} className="rounded-full focus:outline-none"><Image src="/png/left.png" alt="left-icon" width={7} height={12} /></button>
+            <h2 className="text-black font-medium text-sm text-center">{displayTexts[displayTextIndex]}</h2>
+            <button onClick={handleClickRight} className="rounded-full focus:outline-none"><Image src="/png/right.png" alt="right-icon" width={7} height={12} /></button>
+        </div>
+    )
+}
+
+export default Banner;
