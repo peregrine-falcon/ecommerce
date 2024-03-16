125532531b3383e0438f62997f0bb93061c82b8e
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2024-03-17 02:49:48 +0530

    updated

--- app/verification/page.tsx
+++ app/verification/page.tsx
@@ -1,55 +1,55 @@
 import Link from 'next/link';
 
-const verification = () => {
+const Verification = () => {
     return (
-        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-            <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl">Verify your email</h2>
-                <h2 className="text-sm font-normal tracking-tight text-gray-900 sm:text-base">
-                    Enter the 8 digit code you have received on swa***@gmail.com
+        <div className=" bg-white px-6 py-16 lg:px-8">
+            <div className='mx-auto max-w-xl border border-[#C1C1C1] rounded-[20px]'>
+                <h2 className="text-xl font-semibold tracking-tight sm:text-[32px] text-center pt-[39px]">Verify your email</h2>
+                <h2 className="text-sm font-normal tracking-tight text-center text-gray-900 sm:text-base mt-4">
+                    Enter the 8 digit code you have received on <br /> swa***@gmail.com
                 </h2>
-            </div>
 
-            <div>
-                <h3 className='text-base font-normal text-black text-center mt-10'>Code</h3>
-                <div className="flex items-center justify-center mt-4 gap-x-4">
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        7
-                    </p>
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        8
-                    </p>
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        7
-                    </p>
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        8
-                    </p>
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        8
-                    </p>
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        8
-                    </p>
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        8
-                    </p>
-                    <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium text-[#365CCE] border border-[#365CCE] rounded-md">
-                        8
-                    </p>
+                <div className="sm:mx-auto mt-10 max-w-sm mx-4">
+                    <h3 className='text-base font-normal text-black mt-10'>Code</h3>
+                    <div className="flex items-center justify-center mt-2 gap-x-4">
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            7
+                        </p>
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            8
+                        </p>
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            7
+                        </p>
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            8
+                        </p>
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            8
+                        </p>
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            8
+                        </p>
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            8
+                        </p>
+                        <p className="flex items-center justify-center w-10 h-10 text-2xl font-medium border border-[#C1C1C1] rounded-md">
+                            8
+                        </p>
+                    </div>
+
+                    <div className="my-12">
+                        <button
+                            type="submit"
+                            className="block w-full rounded-md bg-black px-3.5 py-2.5 text-base font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            VERIFY
+                        </button>
+                    </div>
                 </div>
-            </div>
-
-            <div className="mt-10">
-                <button
-                    type="submit"
-                    className="block w-full rounded-md bg-black px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    VERIFY
-                </button>
             </div>
         </div>
     )
 }
 
-export default verification
+export default Verification;
